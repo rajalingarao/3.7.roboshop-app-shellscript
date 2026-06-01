b76c7ef91e6cc50b34278be838788f39979659fc
rajalingarao4545 <rajalingarao4545@gmail.com>
2026-06-01 10:36:31 +0530

    roboshop app shellscript

--- dispatch.sh
+++ dispatch.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-# ---------------------------------------
-# Dispatch Service Installation Script
-# ---------------------------------------
-
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(basename "$0" .sh)
 LOGFILE="/tmp/${SCRIPT_NAME}-${TIMESTAMP}.log"
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-# ---------------------------------------
-# Root Check
-# ---------------------------------------
 if [ "$USERID" -ne 0 ]; then
     echo -e "${R}Please run this script as root${N}"
     exit 1
@@ -24,9 +16,6 @@ else
     echo -e "${G}Root user detected${N}"
 fi
 
-# ---------------------------------------
-# Validate Function
-# ---------------------------------------
 VALIDATE() {
     if [ $1 -ne 0 ]; then
         echo -e "$2 ... ${R}FAILED${N}"
@@ -35,18 +24,20 @@ VALIDATE() {
         echo -e "$2 ... ${G}SUCCESS${N}"
     fi
 }
+#Adds the Go workspace binary directory to your system PATH.
+export GOPATH=/app/go
+export PATH=$PATH:$GOPATH/bin
+VALIDATE $? "Adds the Go workspace binary directory to your system PATH"
 
-# ---------------------------------------
-# Install GoLang
-# ---------------------------------------
+#Sets the Go build cache directory to /tmp/go-build-cache.
+export GOCACHE=/tmp/go-build-cache
+mkdir -p $GOCACHE
+VALIDATE $? "Sets the Go build cache directory to /tmp/go-build-cache"
+
 dnf install golang -y &>>"$LOGFILE"
 VALIDATE $? "Installing GoLang"
 
-# ---------------------------------------
-# Create roboshop user (system user)
-# ---------------------------------------
 id roboshop &>>"$LOGFILE"
-
 if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin \
     --comment "roboshop system user" roboshop &>>"$LOGFILE"
@@ -55,32 +46,16 @@ else
     echo -e "roboshop user already exists ... ${Y}SKIPPING${N}"
 fi
 
-# ---------------------------------------
-# Prepare application directory
-# ---------------------------------------
 rm -rf /app &>>"$LOGFILE"
 mkdir -p /app &>>"$LOGFILE"
 VALIDATE $? "Creating /app directory"
 
-# ---------------------------------------
-# Download Dispatch code
-# ---------------------------------------
-curl -L -o /tmp/dispatch.zip \
-https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip &>>"$LOGFILE"
-
+curl -L -o /tmp/dispatch.zip https://roboshop-builds.s3.amazonaws.com/dispatch.zip
 VALIDATE $? "Downloading Dispatch code"
 
-# ---------------------------------------
-# Extract application
-# ---------------------------------------
-cd /app || exit 1
+cd /app
 unzip -o /tmp/dispatch.zip &>>"$LOGFILE"
 VALIDATE $? "Extracting Dispatch code"
-
-# ---------------------------------------
-# Build Go application
-# ---------------------------------------
-cd /app || exit 1
 
 go mod init dispatch &>>"$LOGFILE"
 go mod tidy &>>"$LOGFILE"
@@ -89,17 +64,9 @@ VALIDATE $? "Downloading Go dependencies"
 go build &>>"$LOGFILE"
 VALIDATE $? "Building Dispatch application"
 
-# ---------------------------------------
-# Setup systemd service
-# ---------------------------------------
-cp /home/ec2-user/3.7.roboshop-app-shellscript/dispatch.service \
-/etc/systemd/system/dispatch.service &>>"$LOGFILE"
-
+cp /home/ec2-user/3.7.roboshop-app-shellscript/dispatch.service /etc/systemd/system/dispatch.service &>>"$LOGFILE"
 VALIDATE $? "Copying systemd service file"
 
-# ---------------------------------------
-# Reload and start service
-# ---------------------------------------
 systemctl daemon-reload &>>"$LOGFILE"
 VALIDATE $? "Reloading systemd daemon"
 
@@ -112,114 +79,8 @@ VALIDATE $? "Starting Dispatch service"
 echo -e "${G}Dispatch installation completed successfully${N}"
 echo "Log file: $LOGFILE"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# #!/bin/bash
-
-# USERID=$(id -u)
-# TIMESTAMP=$(date +%F-%H-%M-%S)
-# SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-# LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-
-# R="\e[31m"
-# G="\e[32m"
-# Y="\e[33m"
-# N="\e[0m"
-
-# if [ $USERID -ne 0 ]
-# then
-#    echo -e "$R Please run this script with root access $N"
-#    exit 1
-# else
-#    echo -e " $G You are super user. $N"
-# fi
-
-# VALIDATE(){
-# if [ $1 -ne 0 ]
-# then
-#    echo -e "$2... $R FAILURE $N"
-#    exit 1
-# else
-#    echo -e "$2... $G SUCCESS $N"
-# fi
-# }
-
-# dnf install golang -y &>>$LOGFILE
-# VALIDATE $? "Installing Go Language"
-
-# id roboshop &>>$LOGFILE
-# if [ $? -ne 0 ]
-# then 
-#     useradd roboshop &>>$LOGFILE
-#     VALIDATE $? "Creating roboshop user"
-# else
-#     echo -e "Roboshop user already created..$Y Skipping $N"
-# fi
-
-# rm -rf /app &>>$LOGFILE
-# VALIDATE $? "Clean up existing directory"
-
-# mkdir -p /app &>>$LOGFILE
-# VALIDATE $? "Creating app directory"
-
-# curl -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip &>>$LOGFILE
-# VALIDATE $? "Downloading Dispatch application"
-
-# cd /app &>>$LOGFILE
-# VALIDATE $? "Moving to app directory"
-
-# unzip /tmp/dispatch.zip &>>$LOGFILE
-# VALIDATE $? "extracting Dispatch"
-
-# cd /app &>>$LOGFILE
-# VALIDATE $? "Moving to app directory"
-
-# go mod init dispatch &>>$LOGFILE
-# VALIDATE $? "Initiating dispatch"
-
-# # export GOPATH=/root/go
-# # mkdir -p $GOPATH
-
-# go get &>>$LOGFILE
-# VALIDATE $? "get Dispatch dependencies"
-
-# #Fix 1: Use go mod tidy instead of go get
-# # go mod tidy &>>$LOGFILE
-# # VALIDATE $? "Downloading dependencies"
-
-# go build &>>$LOGFILE
-# VALIDATE $? "build Dispatch"
-
-# cp /home/ec2-user/3.7.roboshop-app-shellscript/dispatch.service /etc/systemd/system/dispatch.service &>>$LOGFILE
-# VALIDATE $? "Copied payment service"
-
-# systemctl daemon-reload &>>$LOGFILE
-# VALIDATE $? "Daemon Reload"
-
-# systemctl enable dispatch &>>$LOGFILE
-# VALIDATE $? "Enable dispatch"
-
-# systemctl start dispatch &>>$LOGFILE
-# VALIDATE $? "start dispatch"
+echo "***************************************"
+sudo systemctl status dispatch
+echo "***************************************"
+ps -ef | grep dispatch
+echo "***************************************"
